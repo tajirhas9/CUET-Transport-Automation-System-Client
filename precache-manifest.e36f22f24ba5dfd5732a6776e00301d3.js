50b119c277c454b5d36dd1d70980fb3e6b97ba1c
Tajir Hasnain <u1604081@student.cuet.ac.bd>
2021-12-26 07:35:12 +0600

    deploy

---
+++ precache-manifest.e36f22f24ba5dfd5732a6776e00301d3.js
@@ -0,0 +1,258 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b46e529cb942f9e009d",
+    "url": "/ctas/css/401.dfe75472.css"
+  },
+  {
+    "revision": "608e2bc26c0ce356787b",
+    "url": "/ctas/css/404.96fe1c26.css"
+  },
+  {
+    "revision": "4a253b03d1607c4d3c5b",
+    "url": "/ctas/css/app.ec4e4d91.css"
+  },
+  {
+    "revision": "e99b40dbd0c57d9524ef",
+    "url": "/ctas/css/chunk-41135466.4e70a2ab.css"
+  },
+  {
+    "revision": "1900c4aa59b755bb5c3f",
+    "url": "/ctas/css/chunk-7c4c6b74.4e70a2ab.css"
+  },
+  {
+    "revision": "7bcf6658c0174ebd9ad2",
+    "url": "/ctas/css/chunk-ff5b103a.4e70a2ab.css"
+  },
+  {
+    "revision": "40b3a41848a7980f584b",
+    "url": "/ctas/css/chunk-libs.35231a94.css"
+  },
+  {
+    "revision": "ddcacc623f7ee855fc21",
+    "url": "/ctas/css/dashboard.138fe784.css"
+  },
+  {
+    "revision": "a9364cfdda2da5655acd",
+    "url": "/ctas/css/login.180dbd06.css"
+  },
+  {
+    "revision": "c73eb1ceba3321a80a0aff13ad373cb4",
+    "url": "/ctas/fonts/Roboto-300.c73eb1ce.woff"
+  },
+  {
+    "revision": "d26871e8149b5759f814fd3c7a4f784b",
+    "url": "/ctas/fonts/Roboto-300.d26871e8.woff2"
+  },
+  {
+    "revision": "fc84e998bc29b297ea20321e4c90b6ed",
+    "url": "/ctas/fonts/Roboto-300.fc84e998.ttf"
+  },
+  {
+    "revision": "35b07eb2f8711ae08d1f58c043880930",
+    "url": "/ctas/fonts/Roboto-400.35b07eb2.woff"
+  },
+  {
+    "revision": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+    "url": "/ctas/fonts/Roboto-400.3e1af3ef.ttf"
+  },
+  {
+    "revision": "73f0a88bbca1bec19fb1303c689d04c6",
+    "url": "/ctas/fonts/Roboto-400.73f0a88b.woff2"
+  },
+  {
+    "revision": "1d6594826615607f6dc860bb49258acb",
+    "url": "/ctas/fonts/Roboto-500.1d659482.woff"
+  },
+  {
+    "revision": "90d1676003d9c28c04994c18bfd8b558",
+    "url": "/ctas/fonts/Roboto-500.90d16760.woff2"
+  },
+  {
+    "revision": "d08840599e05db7345652d3d417574a9",
+    "url": "/ctas/fonts/Roboto-500.d0884059.ttf"
+  },
+  {
+    "revision": "50d75e48e0a3ddab1dd15d6bfb9d3700",
+    "url": "/ctas/fonts/Roboto-700.50d75e48.woff"
+  },
+  {
+    "revision": "b52fac2bb93c5858f3f2675e4b52e1de",
+    "url": "/ctas/fonts/Roboto-700.b52fac2b.woff2"
+  },
+  {
+    "revision": "ee7b96fa85d8fdb8c126409326ac2d2b",
+    "url": "/ctas/fonts/Roboto-700.ee7b96fa.ttf"
+  },
+  {
+    "revision": "16f8179b40f777bec60a720bad68f051",
+    "url": "/ctas/fonts/dxiconsmaterial.16f8179b.woff"
+  },
+  {
+    "revision": "2b7a63682e5cc6c8c06b39e91b54ec0c",
+    "url": "/ctas/fonts/dxiconsmaterial.2b7a6368.woff2"
+  },
+  {
+    "revision": "8a6715d5eddbb0c4788386101dda94bb",
+    "url": "/ctas/fonts/dxiconsmaterial.8a6715d5.ttf"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/ctas/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/ctas/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "089007e721e1f22809c0313b670a36f1",
+    "url": "/ctas/img/401.089007e7.gif"
+  },
+  {
+    "revision": "0f4bc32b0f52f7cfb7d19305a6517724",
+    "url": "/ctas/img/404-cloud.0f4bc32b.png"
+  },
+  {
+    "revision": "a57b6f31fa77c50f14d756711dea4158",
+    "url": "/ctas/img/404.a57b6f31.png"
+  },
+  {
+    "revision": "36beed97f02cfddade0bcd241f5dad33",
+    "url": "/ctas/img/cuet-logo.36beed97.png"
+  },
+  {
+    "revision": "36beed97f02cfddade0bcd241f5dad33",
+    "url": "/ctas/img/cuet-logo.png"
+  },
+  {
+    "revision": "2b02ed4567a65febe8c3a717decb6a29",
+    "url": "/ctas/index.html"
+  },
+  {
+    "revision": "1b46e529cb942f9e009d",
+    "url": "/ctas/js/401.e4c359df.js"
+  },
+  {
+    "revision": "608e2bc26c0ce356787b",
+    "url": "/ctas/js/404.831edd84.js"
+  },
+  {
+    "revision": "4a253b03d1607c4d3c5b",
+    "url": "/ctas/js/app.a303e874.js"
+  },
+  {
+    "revision": "26eb3e5f0df9bc748d29",
+    "url": "/ctas/js/auth-redirect.4c6a489c.js"
+  },
+  {
+    "revision": "bb7ea57e14d23e79cf2b",
+    "url": "/ctas/js/chunk-2c103376.6857356d.js"
+  },
+  {
+    "revision": "e99b40dbd0c57d9524ef",
+    "url": "/ctas/js/chunk-41135466.2a336e9e.js"
+  },
+  {
+    "revision": "1900c4aa59b755bb5c3f",
+    "url": "/ctas/js/chunk-7c4c6b74.6d4f692b.js"
+  },
+  {
+    "revision": "fec3f008072e0ea769c1",
+    "url": "/ctas/js/chunk-ccef37e0.42089e5d.js"
+  },
+  {
+    "revision": "b66a170316157171f68e",
+    "url": "/ctas/js/chunk-elementUI.b4074c0c.js"
+  },
+  {
+    "revision": "7bcf6658c0174ebd9ad2",
+    "url": "/ctas/js/chunk-ff5b103a.5299b1a7.js"
+  },
+  {
+    "revision": "40b3a41848a7980f584b",
+    "url": "/ctas/js/chunk-libs.a5d18776.js"
+  },
+  {
+    "revision": "ddcacc623f7ee855fc21",
+    "url": "/ctas/js/dashboard.7873b3bd.js"
+  },
+  {
+    "revision": "a9364cfdda2da5655acd",
+    "url": "/ctas/js/login.ec46ac3b.js"
+  },
+  {
+    "revision": "707202a60c1d07edd15b",
+    "url": "/ctas/js/redirect.a47fc8db.js"
+  },
+  {
+    "revision": "dfdc22e62e2c8be20a15",
+    "url": "/ctas/js/runtime.822c35d0.js"
+  },
+  {
+    "revision": "65fcf6a87bc26caa3e59",
+    "url": "/ctas/js/vendors~dashboard.2a2f8f08.js"
+  },
+  {
+    "revision": "f8a0103069820156379dd466d9f6bb42",
+    "url": "/ctas/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ctas/robots.txt"
+  },
+  {
+    "revision": "331afb7f9192fadaac7d785a391e39b2",
+    "url": "/ctas/tinymce/README.md"
+  },
+  {
+    "revision": "917c1605513b2a31413fb4a28cf41805",
+    "url": "/ctas/tinymce/emojis.min.js"
+  },
+  {
+    "revision": "833bb31f5d452f0b036f558304f7bd43",
+    "url": "/ctas/tinymce/langs/es.js"
+  },
+  {
+    "revision": "2307d7e18f4d82323f8460b954366d6c",
+    "url": "/ctas/tinymce/langs/it.js"
+  },
+  {
+    "revision": "c23079797eb0bbb399070c37ef52efc9",
+    "url": "/ctas/tinymce/langs/ja.js"
+  },
+  {
+    "revision": "c60c7a4d77abf22380711ea8c1982bb1",
+    "url": "/ctas/tinymce/langs/ko_KR.js"
+  },
+  {
+    "revision": "a561a4484a28c2267c30a4455d3da68e",
+    "url": "/ctas/tinymce/langs/zh_CN.js"
+  },
+  {
+    "revision": "fa6ba7fd4905539e5b2aa845d383278b",
+    "url": "/ctas/tinymce/skins/content.inline.min.css"
+  },
+  {
+    "revision": "33ccf85167a5181c2dead9c10ccfbc4b",
+    "url": "/ctas/tinymce/skins/content.min.css"
+  },
+  {
+    "revision": "411c2608b6be78849a76c0ed14200234",
+    "url": "/ctas/tinymce/skins/content.mobile.min.css"
+  },
+  {
+    "revision": "baecf466c40e709e7ffdbc935fc0813a",
+    "url": "/ctas/tinymce/skins/fonts/tinymce-mobile.woff"
+  },
+  {
+    "revision": "87d30a0b3dd7f30d905aa5a79d64a686",
+    "url": "/ctas/tinymce/skins/skin.min.css"
+  },
+  {
+    "revision": "4fdf33191102d7a24a5bf0639040d128",
+    "url": "/ctas/tinymce/skins/skin.mobile.min.css"
+  },
+  {
+    "revision": "b3b3ae6828c8a28eed8b0b4cceea8f00",
+    "url": "/ctas/tinymce/skins/skin.shadowdom.min.css"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,16 @@
+importScripts("/ctas/precache-manifest.e36f22f24ba5dfd5732a6776e00301d3.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+// This is the code piece that GenerateSW mode can't provide for us.
+// This code listens for the user's confirmation to update the app.
+self.addEventListener('message', (e) => {
+  if (e.data) {
+    if (e.data === 'skipWaiting') {
+      self.skipWaiting()
+    }
+  }
+})
+
+/* eslint-disable no-undef */
+workbox.core.clientsClaim()
+workbox.precaching.precacheAndRoute(self.__precacheManifest || [])
+
